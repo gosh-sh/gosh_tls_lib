4f7d257e71186d8607e5088466c48cd07e6ed4d3
alinaT <alina.t@gosh.sh>
2025-07-10 21:14:20 +0300

    first passenger

---
+++ src/network.rs
@@ -0,0 +1,14 @@
+use std::io::{Write};
+
+pub fn send(conn: &mut dyn Write, buf: &[u8]) { 
+    match conn.write(buf) {
+        Ok(n) => {
+            if n != buf.len() {
+                eprintln!("didn't send all bytes");
+            }
+        }
+        Err(err) => {
+            eprintln!("error in Send: {}", err);
+        }
+    }
+}
